80ff01ea265b8e2e4018ff35a312bce39ae8ffc3
devin-dunn <devineamondunn@live.com>
2023-03-18 16:00:18 -0400

    fixed landscape mobile issues

---
+++ script.js
@@ -0,0 +1,9 @@
+const hamburger = document.getElementById("hamburger");
+
+const hamburgerList = document.getElementById("hamburger-list");
+
+const hamburgerContainer = document.getElementById('hamburger-menu-container');
+
+hamburger.addEventListener("click", function() {
+    
+});
